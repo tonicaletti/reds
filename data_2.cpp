e43f5650ac2aa3c94c794359fe643eaecba7fe6f
tonicaletti <rauc.ma@gmail.com>
2022-02-28 20:09:21 +0100

    comlinux

--- data_2.cpp
+++ data_2.cpp
@@ -3,6 +3,15 @@
 int main()
 {
     std::cout << "New content.";
+    int* x = new int;
+    *x = 5;
+    std::cout << *x;
+
+    double* y = new double;
+    *y = 4.4;
+    std::cout << *y +1;
     std::cout << "Linux";
+    delete x;
+    delete y;
     return 0;
 }
